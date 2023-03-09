cf33b85045f805c12e86299b74dea70e7bfe96a2
bulat <bulatxajrutdinoff@yandex.ru>
2023-03-09 15:20:03 +0300

    saveToFile and newDeckFromFile functions

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ func main() {
 	// var card string = "Ace of Spades" // Туз Пик
 	// card := newCard()
 	// fmt.Println(card)
-	cards := newDeck()
+	// cards := newDeck()
+	cards := newDeckFromFile("my_cards")
+	// cards.saveToFile("my_cards")
 
 	hand, remainingDeck := deal(cards, 5)
+
 	fmt.Println("Hand:")
 	hand.print()
 	fmt.Println("Remaining deck:")
 	remainingDeck.print()
 	// cards.print()
+
 }
